53af2d19a12c736f84d3ee126d27e926af89f6b6
zhenzenghui <1650394096@qq.com>
2020-06-09 18:31:53 +0800

    打印接口耗时好参数 aop

--- src/main/java/com/example/demo/contoller/Test.java
+++ src/main/java/com/example/demo/contoller/Test.java
@@ -1,10 +1,20 @@
 package com.example.demo.contoller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author zzh
  * @date 2020/6/9
  */
+
+@RestController
 public class Test {
 
+    @RequestMapping("/test1/{ss}")
+    public String test1(@PathVariable String ss){
+        return ss;
+    }
 
 }
